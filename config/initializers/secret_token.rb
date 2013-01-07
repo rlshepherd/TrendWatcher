ec8b6ebad37cd8e146bdaafd257220ce26c0ed2a
rlshepherd <russell.l.shepherd@gmail.com>
2013-01-08 01:04:47 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TrendWatcher::Application.config.secret_token = '0c7f4b99154a5889515896715d6a1a068133c6b8de806210bbba92a7dbe85b4de77618f36cca92846e781a69a3db60ebcb40ed11104a529b1d018c051ee2bddf'
